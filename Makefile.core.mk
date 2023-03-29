7ae5b20d19a85e494e88389c5df1742bcddedab0
露营 <luying.yh@alibaba-inc.com>
2023-03-29 12:04:33 +0800

    merge from istio.io/client-go

---
+++ Makefile.core.mk
@@ -0,0 +1,133 @@
+# Copyright Istio Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+default: all
+
+all: clean gen build verify
+
+gen: generate-k8s-client tidy-go mirror-licenses
+
+gen-check: clean gen check-clean-repo
+
+build: build-k8s-client
+
+verify: verify-k8s-client
+
+clean: clean-k8s-client
+
+########################
+# kubernetes code generators
+########################
+applyconfiguration_gen = applyconfiguration-gen
+kubetype_gen = kubetype-gen
+deepcopy_gen = deepcopy-gen
+client_gen = client-gen
+lister_gen = lister-gen
+informer_gen = informer-gen
+
+empty:=
+space := $(empty) $(empty)
+comma := ,
+
+# source packages to scan for kubetype-gen tags
+kube_istio_source_packages = $(subst $(space),$(empty), \
+	istio.io/api/extensions/v1alpha1, \
+	istio.io/api/networking/v1alpha3, \
+	istio.io/api/networking/v1beta1, \
+	istio.io/api/security/v1beta1, \
+	istio.io/api/telemetry/v1alpha1, \
+	istio.io/api/alibabacloud/v1beta1 \
+	)
+
+# base output package for generated files
+kube_base_output_package = istio.io/client-go/pkg
+# base output package for kubernetes types, register, etc...
+kube_api_base_package = $(kube_base_output_package)/apis
+# source packages to scan for kubernetes generator tags, e.g. deepcopy-gen, client-gen, etc.
+# these should correspond to the output packages from kubetype-gen
+kube_api_packages = $(subst $(space),$(empty), \
+	$(kube_api_base_package)/extensions/v1alpha1, \
+	$(kube_api_base_package)/networking/v1alpha3, \
+	$(kube_api_base_package)/networking/v1beta1, \
+	$(kube_api_base_package)/security/v1beta1, \
+	$(kube_api_base_package)/telemetry/v1alpha1, \
+	$(kube_api_base_package)/istio/v1beta1 \
+	)
+# this is needed to properly generate ssa functions
+kube_api_applyconfiguration_packages = $(kube_api_packages),k8s.io/apimachinery/pkg/apis/meta/v1
+# base output package used by kubernetes client-gen
+kube_clientset_package = $(kube_base_output_package)/clientset
+# clientset name used by kubernetes client-gen
+kube_clientset_name = versioned
+# base output package used by kubernetes lister-gen
+kube_listers_package = $(kube_base_output_package)/listers
+# base output package used by kubernetes informer-gen
+kube_informers_package = $(kube_base_output_package)/informers
+# base output package used by kubernetes applyconfiguration-gen
+kube_applyconfiguration_package = $(kube_base_output_package)/applyconfiguration
+
+# file header text
+kube_go_header_text = header.go.txt
+
+ifeq ($(IN_BUILD_CONTAINER),1)
+	# k8s code generators rely on GOPATH, using $GOPATH/src as the base package
+	# directory.  Using --output-base . does not work, as that ends up generating
+	# code into ./<package>, e.g. ./istio.io/client-go/pkg/apis/...  To work
+	# around this, we'll just let k8s generate the code where it wants and copy
+	# back to where it should have been generated.
+	move_generated=cp -r $(GOPATH)/src/$(kube_base_output_package)/ . && rm -rf $(GOPATH)/src/$(kube_base_output_package)/
+else
+	# nothing special for local builds
+	move_generated=
+endif
+
+rename_generated_files=\
+	find $(subst istio.io/client-go/, $(empty), $(subst $(comma), $(space), $(kube_api_packages)) $(kube_clientset_package) $(kube_listers_package) $(kube_informers_package)) \
+	-name '*.go' -and -not -name 'doc.go' -and -not -name '*.gen.go' -type f -exec sh -c 'mv "$$1" "$${1%.go}".gen.go' - '{}' \;
+
+.PHONY: generate-k8s-client
+generate-k8s-client:
+	# generate kube api type wrappers for istio types
+	@KUBETYPE_GOLANG_PROTOBUF=true $(kubetype_gen) --input-dirs $(kube_istio_source_packages) --output-package $(kube_api_base_package) -h $(kube_go_header_text)
+	@$(move_generated)
+	# generate deepcopy for kube api types
+	@$(deepcopy_gen) --input-dirs $(kube_api_packages) -O zz_generated.deepcopy  -h $(kube_go_header_text)
+	# generate ssa for kube api types
+	@$(applyconfiguration_gen) --input-dirs $(kube_api_applyconfiguration_packages) --output-package $(kube_applyconfiguration_package) -h $(kube_go_header_text)
+	# generate clientsets for kube api types
+	@$(client_gen) --clientset-name $(kube_clientset_name) --input-base "" --input  $(kube_api_packages) --output-package $(kube_clientset_package) -h $(kube_go_header_text) --apply-configuration-package $(kube_applyconfiguration_package)
+	# generate listers for kube api types
+	@$(lister_gen) --input-dirs $(kube_api_packages) --output-package $(kube_listers_package) -h $(kube_go_header_text)
+	# generate informers for kube api types
+	@$(informer_gen) --input-dirs $(kube_api_packages) --versioned-clientset-package $(kube_clientset_package)/$(kube_clientset_name) --listers-package $(kube_listers_package) --output-package $(kube_informers_package) -h $(kube_go_header_text)
+	@$(move_generated)
+	@$(rename_generated_files)
+
+.PHONY: build-k8s-client verify-k8s-client
+build-k8s-client:
+	# building k8s client
+	@go build ./pkg/...
+
+verify-k8s-client:
+	# verifying generated client code
+	@go build ./cmd/...
+
+.PHONY: clean-k8s-client
+clean-k8s-client:
+    # remove generated code
+	@rm -rf pkg/
+
+lint: lint-all
+
+include common/Makefile.common.mk
---
+++ go.mod
@@ -0,0 +1,48 @@
+module istio.io/client-go
+
+go 1.17
+
+require (
+	istio.io/api v0.0.0-20221101232721-167a8353d0c0
+	k8s.io/apimachinery v0.24.1
+	k8s.io/client-go v0.23.5
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
+	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/googleapis/gnostic v0.5.5 // indirect
+	github.com/imdario/mergo v0.3.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/net v0.0.0-20220624214902-1bab6f366d9e // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/sys v0.0.0-20220627191245-f75cf1eec38b // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20220628213854-d9e0b6570c03 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	k8s.io/api v0.24.1 // indirect
+	k8s.io/klog/v2 v2.60.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
+)
+
+replace istio.io/api => github.com/aliyun/alibabacloud-servicemesh-api v0.0.0-20230307095419-459851a05db9

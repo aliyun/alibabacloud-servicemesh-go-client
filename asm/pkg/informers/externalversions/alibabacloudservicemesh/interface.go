4decab9612450d8dbdd98b6ddfa6fb79cebdda5d
Kuromesi <blackfacepan@163.com>
2025-04-27 17:55:01 +0800

    update asm clients
    Signed-off-by: Kuromesi <blackfacepan@163.com>

--- asm/pkg/informers/externalversions/alibabacloudservicemesh/interface.go
+++ asm/pkg/informers/externalversions/alibabacloudservicemesh/interface.go
@@ -17,6 +17,7 @@ package alibabacloudservicemesh
 
 import (
 	v1 "istio.io/client-go/asm/pkg/informers/externalversions/alibabacloudservicemesh/v1"
+	v1beta1 "istio.io/client-go/asm/pkg/informers/externalversions/alibabacloudservicemesh/v1beta1"
 	internalinterfaces "istio.io/client-go/asm/pkg/informers/externalversions/internalinterfaces"
 )
 
@@ -24,6 +25,8 @@ import (
 type Interface interface {
 	// V1 provides access to shared informers for resources in V1.
 	V1() v1.Interface
+	// V1beta1 provides access to shared informers for resources in V1beta1.
+	V1beta1() v1beta1.Interface
 }
 
 type group struct {
@@ -40,4 +43,9 @@ func New(f internalinterfaces.SharedInformerFactory, namespace string, tweakList
 // V1 returns a new v1.Interface.
 func (g *group) V1() v1.Interface {
 	return v1.New(g.factory, g.namespace, g.tweakListOptions)
+}
+
+// V1beta1 returns a new v1beta1.Interface.
+func (g *group) V1beta1() v1beta1.Interface {
+	return v1beta1.New(g.factory, g.namespace, g.tweakListOptions)
 }

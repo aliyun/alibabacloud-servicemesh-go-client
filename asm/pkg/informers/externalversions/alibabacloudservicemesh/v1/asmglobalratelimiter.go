eeb4d410f724c53b1131eafda5394c9774e7f5e8
Kuromesi <blackfacepan@163.com>
2025-04-27 17:55:01 +0800

    update asm clients
    Signed-off-by: Kuromesi <blackfacepan@163.com>

--- asm/pkg/informers/externalversions/alibabacloudservicemesh/v1/asmglobalratelimiter.go
+++ asm/pkg/informers/externalversions/alibabacloudservicemesh/v1/asmglobalratelimiter.go
@@ -16,7 +16,7 @@
 package v1
 
 import (
-	clientset "istio.io/client-go/asm/pkg/clientset"
+	versioned "istio.io/client-go/asm/pkg/clientset/versioned"
 	internalinterfaces "istio.io/client-go/asm/pkg/informers/externalversions/internalinterfaces"
 	v1 "istio.io/client-go/asm/pkg/listers/alibabacloudservicemesh/v1"
 	"context"
@@ -45,14 +45,14 @@ type aSMGlobalRateLimiterInformer struct {
 // NewASMGlobalRateLimiterInformer constructs a new informer for ASMGlobalRateLimiter type.
 // Always prefer using an informer factory to get a shared informer instead of getting an independent
 // one. This reduces memory footprint and number of connections to the server.
-func NewASMGlobalRateLimiterInformer(client clientset.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers) cache.SharedIndexInformer {
+func NewASMGlobalRateLimiterInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers) cache.SharedIndexInformer {
 	return NewFilteredASMGlobalRateLimiterInformer(client, namespace, resyncPeriod, indexers, nil)
 }
 
 // NewFilteredASMGlobalRateLimiterInformer constructs a new informer for ASMGlobalRateLimiter type.
 // Always prefer using an informer factory to get a shared informer instead of getting an independent
 // one. This reduces memory footprint and number of connections to the server.
-func NewFilteredASMGlobalRateLimiterInformer(client clientset.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
+func NewFilteredASMGlobalRateLimiterInformer(client versioned.Interface, namespace string, resyncPeriod time.Duration, indexers cache.Indexers, tweakListOptions internalinterfaces.TweakListOptionsFunc) cache.SharedIndexInformer {
 	return cache.NewSharedIndexInformer(
 		&cache.ListWatch{
 			ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
@@ -74,7 +74,7 @@ func NewFilteredASMGlobalRateLimiterInformer(client clientset.Interface, namespa
 	)
 }
 
-func (f *aSMGlobalRateLimiterInformer) defaultInformer(client clientset.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {
+func (f *aSMGlobalRateLimiterInformer) defaultInformer(client versioned.Interface, resyncPeriod time.Duration) cache.SharedIndexInformer {
 	return NewFilteredASMGlobalRateLimiterInformer(client, f.namespace, resyncPeriod, cache.Indexers{cache.NamespaceIndex: cache.MetaNamespaceIndexFunc}, f.tweakListOptions)
 }
 
